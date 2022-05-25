efdefd7b4b0e9f4c2629c3b29e810bffb9ee778d
Harits Syah <haritssr@gmail.com>
2022-05-25 12:03:39 +0700

    delete big and unused static images

--- pages/experiments/nextjs/image-components.tsx
+++ pages/experiments/nextjs/image-components.tsx
@@ -7,26 +7,28 @@ import Link from 'next/link';
 
 const ProductData = [
   { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
-  { id: '2', src: '/new/kemiri.png', name: 'candlenut', href: '/work' },
-  { id: '3', src: '/new/kopi.png', name: 'coffee beans', href: '/work' },
-  { id: '4', src: '/new/jagung.png', name: 'corn', href: '/work' },
-  { id: '5', src: '/new/teh.png', name: 'tea', href: '/work' },
-  { id: '6', src: '/new/pala.png', name: 'pala', href: '/work' },
-  { id: '7', src: '/new/kelapa.png', name: 'coconut', href: '/work' },
-  { id: '8', src: '/new/sayuran.png', name: 'sayuran', href: '/work' },
+  { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
+  { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
+  { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
+  { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
+  { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
+  { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
+  { id: '1', src: '/new/gambir.jpg', name: 'gambier', href: '/work' },
 ];
 
 export default function ImageComponents() {
   return (
     <LayoutToExperiments title='Image Component' domain='Next.js'>
       <SubTitle>My implementation of Next.js Image Components </SubTitle>
-      <main className='xs:grid-cols-2 z-0 mx-auto grid max-w-4xl grid-cols-1 gap-5 py-20 sm:grid-cols-3 lg:grid-cols-4'>
+      <div className=' pb-2 text-center'>Grid Images</div>
+      <main className='z-0 mx-auto grid max-w-4xl grid-cols-1 gap-5 pt-2 pb-20 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4'>
         {ProductData.map(a => (
           <ProductCard key={a.id} href={a.href} src={a.src} name={`${a.name}`} />
         ))}
       </main>
 
       <div>
+        <div className=' pb-2 text-center'>Large Image</div>
         <Image
           alt='Image'
           src='/new/hero.jpg'
@@ -40,21 +42,21 @@ export default function ImageComponents() {
         />
       </div>
 
-      <div className='mx-auto mt-20 flex h-auto max-w-5xl flex-col space-y-10 bg-zinc-400'>
+      <div className='mx-auto mt-20 flex h-auto max-w-5xl flex-col space-y-20'>
         <div>
-          <div className='bg-blue-200 py-3 text-center'>img tag</div>
+          <div className=' pb-2 text-center'>img tag</div>
 
           <img
             alt=''
             src='/new/hero.jpg'
             // height={2000}
             // width={5000}
-            className='h-96 w-full object-cover object-top'
+            className='h-96 w-full rounded-md object-cover object-top'
             placeholder='blur'
           />
         </div>
         <div>
-          <div className='bg-blue-200 py-3 text-center'>next image components</div>
+          <div className=' pb-2 text-center'>next image components</div>
           <div className='relative h-96'>
             <Image
               alt='Image'
@@ -64,7 +66,7 @@ export default function ImageComponents() {
               width='1007'
               objectFit='cover'
               quality={75}
-              className='overflow-hidden rounded-md'
+              className='overflow-hidden rounded-md rounded-md'
               blurDataURL='/new/hero.jpg'
               placeholder='blur'
             />
