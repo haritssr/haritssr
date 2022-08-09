0b41327498732df2168bb050d081e78342062299
Harits Syah <haritssr@gmail.com>
2022-08-09 22:13:30 +0700

    add better explanation on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className=' text-[12px] sm:text-tiny relative mt-20 sm:border-zinc-200 px-8 pt-3 pb-3 text-center xl:px-0'>
+    <footer className=' relative mt-20 px-8 pt-3 pb-3 text-center text-[12px] sm:border-zinc-200 sm:text-tiny xl:px-0'>
       <section>
         <Link href='/'>
           <a className=' text-zinc-500 duration-100 hover:text-zinc-800 '>
-            &copy; Harits Syah{' '}
-            <span className='text-[12px] sm:text-tiny'>2021–{new Date().getFullYear()}</span>
+            harits<span className='text-zinc-700 font-semibold'>sr</span>.com by Harits Syah &copy;
+            <span className='text-[12px] sm:text-tiny'> 2021–{new Date().getFullYear()}</span>
           </a>
         </Link>
       </section>
