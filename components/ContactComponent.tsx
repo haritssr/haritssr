f09a2e100bab27c4f9e6868d8ac25086dae172f1
Harits Syah <haritssr@gmail.com>
2022-08-11 03:47:37 +0700

    redesign for better discoverability of these buttons

--- components/ContactComponent.tsx
+++ components/ContactComponent.tsx
@@ -1,8 +1,6 @@
 import { ExternalLinkIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 
-//this componenets used in tech stack, and contact section
-
 export default function ContactComponent({
   href,
   imgSrc,
@@ -20,14 +18,14 @@ export default function ContactComponent({
       href={href}
       target='_blank'
       rel='noreferrer noopener'
-      className='group flex w-auto items-center justify-between space-x-2 rounded-lg  bg-zinc-50 py-2 px-3 sm:px-5 hover:bg-gray-100 border border-zinc-300 shadow'
+      className='group flex w-auto items-center justify-between space-x-2 rounded-lg  border border-zinc-300 bg-white py-2 px-3 shadow  hover:border-zinc-800 hover:ring-2 hover:ring-zinc-300 sm:px-4'
     >
-      <section className='flex items-center space-x-2 overflow-hidden'>
+      <section className='flex items-center space-x-2 overflow-hidden sm:space-x-3'>
         <Image src={imgSrc} height={20} width={20} alt={name} />
         <div className='truncate font-medium text-zinc-700'>{name}</div>
       </section>
       <ExternalLinkIcon
-        className='h-5 w-5 pt-0.5 text-zinc-500 group-hover:text-zinc-700'
+        className='h-5 w-5 pt-0.5 text-zinc-500 group-hover:stroke-2 group-hover:text-zinc-700'
         strokeWidth={1.5}
       />
     </a>
