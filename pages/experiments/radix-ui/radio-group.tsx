0cbb66d8ce326363f11f6f5bed7deb864b711013
Harits Syah <haritssr@gmail.com>
2022-08-18 13:08:57 +0700

    re-install mantine

--- pages/experiments/radix-ui/radio-group.tsx
+++ pages/experiments/radix-ui/radio-group.tsx
@@ -9,16 +9,20 @@ export default function RadioGroupDemo() {
         A set of checkable buttons—known as radio buttons—where no more than one of the buttons can
         be checked at a time.
       </SubTitle>
-      <RadioGroup.Root defaultValue='default '>
+      <RadioGroup.Root className=''>
         <RadioGroup.Item
-          value='balskdjf'
+          value='default'
           id='r1'
           className='flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-lg'
         >
-          <RadioGroup.Indicator className='relative h-full w-full after:block after:h-4 after:w-4 after:rounded-full after:bg-violet-500' />
-          <label className='pl-5 text-lg text-gray-700' htmlFor='r2'>
-            Comfortable
-          </label>
+          <RadioGroup.Indicator className='h-4 w-4 rounded-full bg-zinc-500' />
+        </RadioGroup.Item>
+        <RadioGroup.Item
+          value='default'
+          id='r1'
+          className='flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-lg'
+        >
+          <RadioGroup.Indicator className='h-4 w-4 rounded-full bg-zinc-500' />
         </RadioGroup.Item>
       </RadioGroup.Root>
     </LayoutToExperiments>
--- pages/experiments/radix-ui/switch.tsx
+++ pages/experiments/radix-ui/switch.tsx
@@ -2,61 +2,29 @@ import React from 'react';
 import { styled } from '@stitches/react';
 import { violet, mauve, blackA, whiteA } from '@radix-ui/colors';
 import * as SwitchPrimitive from '@radix-ui/react-switch';
-import { SubTitle } from '@/components/DesignSystem';
+import { ExternalLink, SubTitle } from '@/components/DesignSystem';
 import LayoutToExperiments from '@/components/LayoutToExperiments';
-
-const StyledSwitch = styled(SwitchPrimitive.Root, {
-  all: 'unset',
-  width: 42,
-  height: 25,
-  backgroundColor: blackA.blackA9,
-  borderRadius: '9999px',
-  position: 'relative',
-  boxShadow: `0 2px 10px ${blackA.blackA7}`,
-  WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
-  '&:focus': { boxShadow: `0 0 0 2px black` },
-  '&[data-state="checked"]': { backgroundColor: 'black' },
-});
-
-const StyledThumb = styled(SwitchPrimitive.Thumb, {
-  display: 'block',
-  width: 21,
-  height: 21,
-  backgroundColor: 'white',
-  borderRadius: '9999px',
-  boxShadow: `0 2px 2px ${blackA.blackA7}`,
-  transition: 'transform 100ms',
-  transform: 'translateX(2px)',
-  willChange: 'transform',
-  '&[data-state="checked"]': { transform: 'translateX(19px)' },
-});
-
-// Exports
-const Switch = StyledSwitch;
-const SwitchThumb = StyledThumb;
-
-// Your app...
-const Flex = styled('div', { display: 'flex' });
-const Label = styled('label', {
-  color: 'black',
-  fontSize: 15,
-  lineHeight: 1,
-  userSelect: 'none',
-});
+import ExplanationList from '@/components/ExplanationList';
+import * as Switch from '@radix-ui/react-switch';
 
 export default function ExportedSwitch() {
   return (
     <LayoutToExperiments title='Switch' domain='Radix UI'>
-      <SubTitle>A control that allows the user to toggle between checked and not checked.</SubTitle>
+      <SubTitle>
+        <ExternalLink
+          href='https://www.radix-ui.com/docs/primitives/components/switch'
+          name='Radix UI Switch'
+        />
+        <ExplanationList>
+          <li>A control that allows the user to toggle between checked and not checked.</li>
+          <li>Click to change state.</li>
+        </ExplanationList>
+      </SubTitle>
       <form>
-        <Flex css={{ alignItems: 'center' }}>
-          <Label htmlFor='s1' css={{ paddingRight: 15 }}>
-            Airplane mode
-          </Label>
-          <Switch defaultChecked id='s1'>
-            <SwitchThumb />
-          </Switch>
-        </Flex>
+        <label>Airplane mode</label>
+        <Switch.Root defaultChecked id='s1' className='block h-5 w-5 bg-zinc-400 shadow-lg'>
+          <Switch.Thumb className='rdx-state-checked:translate-x-10 block h-2 w-3 rounded-full bg-rose-500 will-change-transform' />
+        </Switch.Root>
       </form>
     </LayoutToExperiments>
   );
