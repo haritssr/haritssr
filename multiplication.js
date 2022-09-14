f5c55381431b74616ce019f9539c03ae8dae1e3a
Harits Syah <haritssr@gmail.com>
2022-09-15 04:09:24 +0700

    add: some jest file & config

---
+++ multiplication.js
@@ -0,0 +1,5 @@
+function multiplication(a, b) {
+  return a * b;
+}
+
+module.exports = multiplication;
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum');
+const multiplication = require('./multiplication')
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('multiply 2 and 4 to equal 8', () => {
+  expect(multiplication(2,4)).toBe(8)
+})j
