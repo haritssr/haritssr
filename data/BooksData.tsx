78b6629b329860b6ab4d26923c64a5078b9d0e9d
Harits Syah <haritssr@gmail.com>
2022-10-01 18:29:46 +0700

    refactor: reading section in index.tsx

--- data/BooksData.tsx
+++ data/BooksData.tsx
@@ -4,28 +4,23 @@ export const TechicalBooksData = [
   {
     title: 'Eloquent JavaScript',
     href: 'https://eloquentjavascript.net/',
-    imgSrc: '/books/eloquent_javascript.jpg',
   },
   {
     title: 'Effective TypeScript',
     href: 'https://effectivetypescript.com/',
-    imgSrc: '/books/effective_typescript.jpg',
   },
   {
     title: 'Grokking Simplicity',
     href: 'https://grokkingsimplicity.com/',
-    imgSrc: '/books/grokking_simplicity.png',
   },
   {
     title: 'Learn Patterns',
     href: 'https://www.patterns.dev/',
-    imgSrc: '/books/learn_patterns.png',
   },
 
   {
     title: 'JavaScript - The Definitive Guide',
     href: 'https://www.oreilly.com/library/view/javascript-the-definitive/9781491952016/',
-    imgSrc: '/books/javascript_the_definitive_guide.jpg',
   },
 ];
 
@@ -33,30 +28,53 @@ export const MathPhysicsBooks = [
   {
     title: 'Math(s) Fix',
     href: 'https://www.computerbasedmath.org/the-maths-fix/',
-    imgSrc: '/books/mathfix.jpg',
   },
   {
     title: 'Quanta Magazine',
     href: 'https://www.quantamagazine.org',
-    imgSrc: '/books/quantamagazine.webp',
   },
   {
     title: 'Increment',
     href: 'https://www.increment.com/',
-    imgSrc: '/books/increment.jpg',
   },
 ];
 export const UIUXBooksData = [
   {
     title: 'Refactoring UI',
     href: 'https://www.refactoringui.com/',
-    imgSrc: '/books/refactoring_ui.png',
   },
-  { title: 'Laws of UX', href: 'https://lawsofux.com/', imgSrc: '/books/laws_of_ux.jpg' },
+  { title: 'Laws of UX', href: 'https://lawsofux.com/' },
 
   {
     title: 'Apple - Human Interface Design',
     href: 'https://developer.apple.com/design/human-interface-guidelines/',
-    imgSrc: '/books/apple_hig.png',
+  },
+];
+
+export const OthersBookData = [
+  {
+    title: 'Mozilla Developer Network',
+    href: 'https://developer.mozilla.org/en-US/',
+  },
+  { title: 'JavaScript.info', href: 'https://javascript.info/' },
+  { title: 'FreeCodeCamp', href: 'https://javascript.info/' },
+  { title: 'HackerRank', href: 'https://www.hackerrank.com/' },
+  { title: 'LeetCode', href: 'https://leetcode.com/' },
+  { title: 'SoloLearn', href: 'https://www.sololearn.com/' },
+  {
+    title: 'Jeff Zhang - OOP with TypeScript',
+    href: 'https://www.youtube.com/watch?v=HsWKyERYGKQ&list=PLn4fTSbSpY5eY_M1mKWxGa9pViEodofKy',
+  },
+  {
+    title: 'Learn Patterns - Web Design Pattern',
+    href: 'https://www.patterns.dev/posts/#design-patterns',
+  },
+  {
+    title: 'Learn Patterns - Web Rendering Pattern',
+    href: 'https://www.patterns.dev/posts/#rendering-patterns',
+  },
+  {
+    title: 'Learn Patterns - Web Performance Pattern',
+    href: 'https://www.patterns.dev/posts/#performance-patterns',
   },
 ];
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,22 +19,20 @@ import {
 import { ContactData } from '../data/ContactData';
 import ProjectsBox from '@/components/ProjectBox';
 import { ProjectsData } from '../data/ProjectsData';
-import { MathPhysicsBooks, TechicalBooksData, UIUXBooksData } from 'data/BooksData';
+import { MathPhysicsBooks, TechicalBooksData, UIUXBooksData, OthersBookData } from 'data/BooksData';
 import React from 'react';
-import { LocationMarkerIcon } from '@heroicons/react/solid';
 import { TechStackComponent } from '@/components/TechStackComponents';
 import ExperimentsIndexBox from '@/components/ExperimentsIndexBox';
 import { ExperimentIndexData } from 'data/ExperimentsIndexData';
-import DialogButton from '@/components/DialogButton';
 
 export default function Home() {
   return (
     <Layout browserTitle='Projects' description='Home'>
-      <div className='mt-5 space-y-28 sm:mt-10 sm:space-y-32'>
+      <div className='mt-5 space-y-20 sm:mt-10 '>
         <Contacts />
         <Projects />
         <Experiments />
-        <Blog />
+        {/* <Blog /> */}
         <TechStack />
         <Reading />
       </div>
@@ -44,13 +42,11 @@ export default function Home() {
 
 const Wrapper = ({
   topic,
-  subtitle,
   className,
   children,
   id,
 }: {
   topic: string;
-  subtitle: string;
   className?: string;
   children: React.ReactNode;
   id: string;
@@ -58,11 +54,7 @@ const Wrapper = ({
   return (
     <div id={id}>
       <div className='mb-6 space-y-2'>
-        <div className='flex items-center'>
-          <span className='text-2xl font-semibold text-zinc-800'>
-            {topic}.<span className='ml-1 text-2xl font-medium text-zinc-500 '>{subtitle}</span>
-          </span>
-        </div>
+        <span className='text-2xl font-semibold text-zinc-800 sm:text-3xl'>{topic}</span>
       </div>
       <div className={className}>{children}</div>
     </div>
@@ -73,50 +65,40 @@ const Contacts = () => {
   return (
     <div
       id='contacts'
-      className='grid grid-cols-1 gap-10 rounded-lg border border-zinc-200 bg-gradient-to-t from-zinc-100 via-white to-white px-5 py-10 shadow sm:grid-cols-2 sm:gap-0 sm:bg-gradient-to-bl sm:via-zinc-50 sm:p-10'
+      className='grid grid-cols-1 gap-5 bg-white px-5 pt-5 sm:grid-cols-3 sm:px-0 sm:pt-10 lg:grid-cols-4'
     >
-      <section className='mx-auto flex flex-col sm:mx-0 sm:mr-10  sm:px-0 sm:pl-4'>
-        <article className=' mb-5 flex justify-center object-center sm:flex sm:h-auto sm:items-center sm:justify-start'>
-          <Image
-            src='/images/me.jpg'
-            height='180'
-            width='180'
-            className='z-10 rounded-full'
-            alt='Harits Syah'
-            blurDataURL='/images/me.jpg'
-            placeholder='blur'
-          />
-        </article>
-        <article className='space-y-2 text-center sm:text-left'>
-          <div className='text-2xl font-bold text-zinc-800'>Harits Syah</div>
-          <div className='flex items-center space-x-1 text-lg text-zinc-500'>
-            <div className='div'>South Tangerang, Indonesia</div>{' '}
-            <a
-              href='https://goo.gl/maps/zNCvRABE5bfJHPcbA'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <LocationMarkerIcon className='h-4 w-4 text-rose-500 hover:text-rose-600 hover:ring-1 hover:ring-rose-300 sm:h-5 sm:w-5' />
-            </a>
-          </div>
-          <div className='text-lg text-zinc-500'>Web Frontend React Engineer</div>
-          <div className='flex justify-center sm:justify-start'>
-            <ExternalLink big={true} name='Haris Lab' href='https://www.harislab.com' />
-          </div>
-        </article>
+      <section className=''>
+        <Image
+          src='/images/me.jpg'
+          height='150'
+          width='150'
+          className='z-10 rounded-full '
+          alt='Harits Syah'
+          blurDataURL='/images/me.jpg'
+          placeholder='blur'
+        />
       </section>
-      <section className='px-5 sm:flex sm:justify-end sm:px-0'>
-        <div className='grid  grid-cols-1 gap-4'>
-          {ContactData.map(c => (
-            <ContactComponent
-              href={c.href}
-              name={c.name}
-              key={c.imgSrc}
-              imgSrc={c.imgSrc}
-              title={c.title}
-            />
-          ))}
+      <section className='space-y-3'>
+        <div className='font-semibold text-zinc-800'>Harits Syah</div>
+        <div className=' text-zinc-400'>Frontend Engineer</div>
+        <div className=' text-zinc-400'>
+          Founder,{' '}
+          <span className='cursor-pointer font-semibold text-zinc-500 hover:underline'>
+            Haris Lab
+          </span>
         </div>
+        <div className=' text-zinc-400'>Tangerang, Indonesia</div>
+      </section>
+      <section className='space-y-3'>
+        {ContactData.map(c => (
+          <ContactComponent
+            href={c.href}
+            name={c.name}
+            key={c.imgSrc}
+            imgSrc={c.imgSrc}
+            title={c.title}
+          />
+        ))}
       </section>
     </div>
   );
@@ -127,8 +109,7 @@ const Projects = () => {
     <Wrapper
       id='projects'
       topic='Projects'
-      subtitle='Hosted frontend projects.'
-      className='grid grid-cols-1 gap-8 px-10 sm:grid-cols-3 sm:gap-5 sm:px-0 lg:grid-cols-4'
+      className='grid grid-cols-1 gap-8 px-5  sm:grid-cols-3 sm:gap-5 sm:px-0 lg:grid-cols-4'
     >
       {ProjectsData.map(d => (
         <ProjectsBox
@@ -151,15 +132,14 @@ const TechStack = () => {
     <Wrapper
       id='techstack'
       topic='Tech Stack'
-      subtitle='Technology I have used.'
-      className='grid grid-flow-row grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4'
+      className='grid grid-flow-row grid-cols-2 gap-5 sm:grid-cols-3 lg:grid-cols-4'
     >
       <TechStackComponent data={Backend} title='Backend' />
       <TechStackComponent data={DataVisulization} title='Data Visualization' />
       <TechStackComponent data={Animation} title='Animation' />
       <TechStackComponent data={Hosting} title='Hosting' />
       <TechStackComponent data={ProgrammingLanguage} title='Programming Language' />
-      <TechStackComponent data={FrontendFramework} title='Frontend Framework' />
+      <TechStackComponent data={FrontendFramework} title='Framework' />
       <TechStackComponent data={Documentation} title='Documentation' />
       <TechStackComponent data={Testing} title='Testing' />
       <TechStackComponent data={ComponentLibrary} title='Component Library' />
@@ -175,7 +155,6 @@ const Experiments = () => {
     <Wrapper
       id='experiments'
       topic='Experiments'
-      subtitle='My tech stack exploration'
       className='grid grid-cols-2 gap-5 sm:grid-cols-3 lg:grid-cols-4 '
     >
       {ExperimentIndexData.map(({ title, href, imgSrc, sum, type, ver }) => (
@@ -195,198 +174,49 @@ const Experiments = () => {
 
 const Blog = () => {
   return (
-    <Wrapper id='blog' topic='Blog' subtitle='A place to pour my thoughts.' className='space-y-4'>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
-      <div>
-        <div className='cursor-pointer text-zinc-700 hover:underline'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, natus?
-        </div>
-        <div className='text-zinc-400'>100k Views</div>
-      </div>
+    <Wrapper id='blog' topic='Blog' className='space-y-4'>
+      <div className=''></div>
     </Wrapper>
   );
 };
 const Reading = () => {
   return (
-    <div id='reading'>
-      <div className=''>
-        <span className='text-2xl font-semibold text-zinc-800 sm:text-3xl'>Reading. </span>
-        <span className='text-2xl font-medium text-zinc-500 sm:text-3xl'>
-          I reading and enjoy re-reading these.
-        </span>
-      </div>
-      <DialogButton
-        titleButton='What does these book means?'
-        titleDescription='
-        The list of books below means:'
-      >
-        <li>I have read those books</li>
-        <li>I love thems so I reread regularly</li>
-        <li>You can discuss with me about the content of the book</li>
-        <li>
-          These books are my source of inspiration when i building user interface and the user
-          experience around it
-        </li>
-        <li>
-          I&#39;m ready to be asked and have the responsibility to be criticized for the knowledge
-          of these books
-        </li>
-      </DialogButton>
-      <div className='rounded-md border border-zinc-200  p-5'>
-        <h2 className='mb-3 text-2xl font-semibold text-zinc-700'>Technical Frontend Books</h2>
-        <section className='grid w-fit grid-flow-row grid-cols-2 gap-5 rounded-md  sm:grid-cols-3 sm:gap-5 sm:gap-x-5 sm:pl-10 md:grid-cols-5'>
-          {TechicalBooksData.map(({ title, href, imgSrc }) => (
-            <div className=' flex h-full flex-col rounded-md ' key={title}>
-              <a
-                title={title}
-                href={href}
-                target='_blank'
-                rel='noreferrer noopener'
-                className='inline-block h-full'
-              >
-                <Image src={imgSrc} alt={title} height={190} width={150} />
-                <div className='text-[12px] text-zinc-500 sm:text-sm'>{title}</div>
-              </a>
-            </div>
+    <Wrapper id='Reading' topic='Reading' className='columns-1 sm:columns-2 sm:gap-5'>
+      <section>
+        <h2 className='mb-3 text-xl font-semibold text-zinc-700'>Technical Frontend Books</h2>
+        <section className='grid grid-cols-1 gap-2.5'>
+          {TechicalBooksData.map(({ title, href }) => (
+            <ExternalLink name={title} href={href} key={title} />
           ))}
         </section>
-        <h2 className='mt-10 mb-3 text-2xl font-semibold text-zinc-700'>
-          User Interface & User Experience Books
-        </h2>
-        <section className='grid w-fit grid-flow-row grid-cols-2 gap-5 rounded-md  sm:grid-cols-3 sm:gap-5 sm:gap-x-5 sm:pl-10 md:grid-cols-5'>
-          {UIUXBooksData.map(({ title, href, imgSrc }) => (
-            <div className='border-zinc-2s00 flex h-full flex-col ' key={title}>
-              <a
-                title={title}
-                href={href}
-                target='_blank'
-                rel='noreferrer noopener'
-                className='inline-block h-full'
-              >
-                <Image src={imgSrc} alt={title} height={190} width={150} />
-                <div className=' text-[12px] text-zinc-500 sm:text-sm'>{title}</div>
-              </a>
-            </div>
+      </section>
+
+      <section>
+        <h2 className='mt-6 mb-3 text-xl font-semibold text-zinc-700'>UI/UX Books</h2>
+        <section className='grid grid-cols-1 gap-2.5'>
+          {UIUXBooksData.map(({ title, href }) => (
+            <ExternalLink name={title} href={href} key={title} />
           ))}
         </section>
-        <h2 className='mt-10 mb-3 text-2xl font-semibold text-zinc-700'>Math & Physics Books</h2>
-        <section className='grid w-fit grid-flow-row grid-cols-2 gap-5 rounded-md  sm:grid-cols-3 sm:gap-5 sm:gap-x-5 sm:pl-10 md:grid-cols-5'>
-          {MathPhysicsBooks.map(({ title, href, imgSrc }) => (
-            <div className='border-zinc-2s00 flex h-full flex-col ' key={title}>
-              <a
-                title={title}
-                href={href}
-                target='_blank'
-                rel='noreferrer noopener'
-                className='inline-block h-full'
-              >
-                <Image src={imgSrc} alt={title} height={190} width={150} />
-                <div className='text-[12px] text-zinc-500 sm:text-sm'>{title}</div>
-              </a>
-            </div>
+      </section>
+
+      <section>
+        <h2 className='mt-6 mb-3 text-xl font-semibold text-zinc-700'>Math & Physics Books</h2>
+        <section className='grid grid-cols-1 gap-2.5'>
+          {MathPhysicsBooks.map(({ title, href }) => (
+            <ExternalLink name={title} href={href} key={title} />
           ))}
         </section>
-        <h2 className='mt-10 mb-3 text-2xl font-semibold text-zinc-700'>
-          My Other Learning Sources
-        </h2>
-        <section className='flex flex-col space-y-2.5 sm:pl-10'>
-          {[
-            { name: 'Mozilla Developer Network', href: 'https://developer.mozilla.org/en-US/' },
-            { name: 'JavaScript.info', href: 'https://javascript.info/' },
-            { name: 'FreeCodeCamp', href: 'https://javascript.info/' },
-            { name: 'HackerRank', href: 'https://www.hackerrank.com/' },
-            { name: 'LeetCode', href: 'https://leetcode.com/' },
-            { name: 'SoloLearn', href: 'https://www.sololearn.com/' },
-          ].map(({ name, href }) => (
-            <ExternalLink key={name} name={name} href={href} />
+      </section>
+
+      <section>
+        <h2 className='mt-6 mb-3 text-xl font-semibold text-zinc-700'>Others</h2>
+        <section className='grid grid-cols-1 gap-2.5'>
+          {OthersBookData.map(({ title, href }) => (
+            <ExternalLink key={title} name={title} href={href} />
           ))}
         </section>
-        <h2 className='mt-10 mb-3 text-2xl font-semibold text-zinc-700'>
-          My Computer Science Concepts Learning Sources
-        </h2>
-        <section className='flex flex-col  sm:pl-10'>
-          <ul className='list-outside list-disc space-y-2.5 pl-4'>
-            {[
-              {
-                concept: 'Functional Programming',
-                book: 'Grokking Simplicity',
-                href: 'https://developer.mozilla.org/en-US/',
-              },
-              {
-                concept: 'Object Oriented Programming with TypeScript',
-                book: 'Jeff Zhang',
-                href: 'https://www.youtube.com/watch?v=HsWKyERYGKQ&list=PLn4fTSbSpY5eY_M1mKWxGa9pViEodofKy',
-              },
-              {
-                concept: 'Web Design Pattern',
-                book: 'Learn Patterns',
-                href: 'https://www.patterns.dev/posts/#design-patterns',
-              },
-              {
-                concept: 'Web Rendering Pattern',
-                book: 'Learn Patterns',
-                href: 'https://www.patterns.dev/posts/#rendering-patterns',
-              },
-              {
-                concept: 'Web Performance Pattern',
-                book: 'Learn Patterns',
-                href: 'https://www.patterns.dev/posts/#performance-patterns',
-              },
-            ].map(({ concept, book, href }) => (
-              <li key={concept}>
-                <span>{concept}</span> <span>—</span> <ExternalLink name={book} href={href} />
-              </li>
-            ))}
-          </ul>
-        </section>
-      </div>
-    </div>
+      </section>
+    </Wrapper>
   );
 };
