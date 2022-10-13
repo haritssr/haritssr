9d4425f51591683baf46bd3a7d8d2bc507674ae6
Harits Syah <haritssr@gmail.com>
2022-10-13 10:46:40 +0700

    [delete] next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import { IdProvider } from '@radix-ui/react-id';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <IdProvider>
       <Component {...pageProps} />
     </IdProvider>
   );
 }
-
-
