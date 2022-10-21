d2860f8efab21fe1a858b6abc92009c403b5c2f1
Harits Syah <haritssr@gmail.com>
2022-10-21 17:27:49 +0700

    [add] basic react-query example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
-import '../styles/globals.css';
-import 'tailwindcss/tailwind.css';
 import { IdProvider } from '@radix-ui/react-id';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
+
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <IdProvider>
-      <Component {...pageProps} />
-    </IdProvider>
+    <QueryClientProvider client={queryClient}>
+      {/*<React.StrictMode> //disabled for react-dnd preview bug for now */}
+      <DndProvider backend={HTML5Backend}>
+        <IdProvider>
+          <Component {...pageProps} />
+        </IdProvider>
+      </DndProvider>
+    </QueryClientProvider>
   );
 }
