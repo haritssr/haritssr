bba0347086000c7ba4d05a0f1a080c06d2844db7
Harits Syah <haritssr@gmail.com>
2022-10-21 17:38:41 +0700

    [add] sidebar to <LayoutToExperiments/>

--- components/LayoutToExperiments.tsx
+++ components/LayoutToExperiments.tsx
@@ -1,16 +1,13 @@
+import { ChevronLeftIcon } from '@heroicons/react/24/outline';
 import Head from 'next/head';
-import GlobalHead from './GlobalHead';
-import Footer from './Footer';
-import React from 'react';
-import { ChevronDownIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
-import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { GitHubIcon } from './Icons';
+import { useRouter } from 'next/router';
+import React from 'react';
 import { useInView } from 'react-intersection-observer';
-import { ExperimentsData } from 'data/ExperimentsData';
-import * as Accordion from '@radix-ui/react-accordion';
-import Image from 'next/future/image';
-import { InternalLink } from './DesignSystem';
+import Footer from './Footer';
+import GlobalHead from './GlobalHead';
+import { GitHubIcon } from './Icons';
+import SideBarExperiments from './SideBarExperiments';
 
 interface LayoutToExperimentsType {
   children: React.ReactNode;
@@ -27,7 +24,7 @@ export default function LayoutToExperiments({ children, title, domain }: LayoutT
   const { ref, inView } = useInView({ rootMargin: '-90px' });
 
   return (
-    <div className='bg-white'>
+    <>
       <Head>
         <title>{yes}</title>
         <link rel='icon' href='/icons/haritssr.svg' />
@@ -44,94 +41,62 @@ export default function LayoutToExperiments({ children, title, domain }: LayoutT
         <meta name='twitter:title' content={title} />
         <meta name='twitter:description' content={title} />
         <meta name='twitter:image' content={image} />
+        <link rel='manifest' href='/static/favicons/site.webmanifest' />
       </Head>
 
       <GlobalHead />
 
-      <div
-        className={`${
-          router.asPath === '/' ? '' : 'sticky top-0'
-        } z-40 mb-5 w-full border-b  border-zinc-200 bg-white/80 py-2 saturate-150 backdrop-blur`}
-      >
-        <section className='mx-auto flex max-w-5xl items-center justify-between px-3 lg:px-0'>
-          <BackToExperiments />
-          <PageTitle inView={inView} title={title} />
-          <PageSource href={githubRoute} />
-        </section>
-      </div>
-      <main className='mx-auto flex max-w-5xl px-5 sm:gap-10 lg:px-0'>
-        <aside className='top-10 mt-5 hidden sm:sticky sm:block sm:w-1/4'>
-          <aside className='space-y-2 rounded-md bg-apple-gray6 p-2'>
-            <div className='mb-3 pl-2 text-2xl font-semibold text-zinc-800'>Experiments</div>
-            {ExperimentsData.map(({ id, title, logoSrc, links }) => (
-              <Accordion.Root
-                type='multiple'
-                key={id}
-                className=' rounded-md hover:bg-white hover:shadow '
-              >
-                <Accordion.Item value='id'>
-                  <Accordion.Header className='rounded-t-md px-2 py-1 rdx-state-open:bg-white rdx-state-open:shadow'>
-                    <Accordion.Trigger className='group flex w-full items-center justify-between space-x-1'>
-                      <div className='flex items-center space-x-2'>
-                        <div className=''>
-                          <Image src={logoSrc} height={16} width={16} alt={title} />
-                        </div>
-                        <span>{title}</span>
-                      </div>
-                      <ChevronDownIcon
-                        strokeWidth={2}
-                        className='h-4 w-4 text-zinc-500 group-rdx-state-open:rotate-180'
-                      />
-                    </Accordion.Trigger>
-                  </Accordion.Header>
-                  <Accordion.Content className='mb-2 space-y-1.5 rounded-b-md px-3 pt-1 pb-2 shadow rdx-state-open:bg-white'>
-                    {links.map(({ name }) => (
-                      <InternalLink
-                        key={name}
-                        href={`/experiments/${title.toLowerCase().replace(' ', '-')}/${name
-                          .toLowerCase()
-                          // /\s/g regex -> search all (g = global) whitespace, and replace them with '-'
-                          .replace(/\s/g, '-')}`}
-                        name={name}
-                      />
-                    ))}
-                  </Accordion.Content>
-                </Accordion.Item>
-              </Accordion.Root>
-            ))}
-          </aside>
+      <main className='mx-auto flex max-w-5xl sm:mt-10 sm:gap-10 lg:px-0'>
+        <aside className='top-10  hidden sm:sticky sm:block sm:w-1/4'>
+          <SideBarExperiments />
         </aside>
-        <article className='mx-auto min-h-screen max-w-4xl sm:w-3/4'>
-          <div className='mt-5 mb-2 w-fit rounded-full border border-zinc-400 px-3 pt-0.5 pb-1 text-sm font-semibold text-zinc-700'>
-            {domain}
-          </div>
-          <h1
-            className='z-40 mx-auto mb-2 block h-auto w-full break-words text-left text-3xl font-bold text-zinc-800'
-            ref={ref}
+        <article className='mx-auto min-h-screen w-full max-w-4xl sm:w-3/4 sm:px-0'>
+          {/* Navigation */}
+          <div
+            className={`${router.asPath === '/' ? '' : 'sticky -top-0.5'} ${
+              inView ? '' : 'border-b'
+            } z-40 mb-5  w-full border-apple-gray4 bg-white py-2 sm:rounded sm:border`}
           >
-            {title}
-          </h1>
-          {children}
+            <div className='mx-auto flex max-w-5xl items-center justify-between px-3 lg:px-5'>
+              <BackToExperiments />
+              <PageTitleCenter inView={inView} title={title} />
+              <PageSource href={githubRoute} />
+            </div>
+          </div>
+
+          {/* Header */}
+          <div className='px-5 sm:px-0'>
+            <div className='mt-5 mb-2 w-fit rounded-full border border-zinc-300 px-3 pb-0.5 pt-1 text-[12px] font-semibold text-zinc-800 sm:mt-10'>
+              {domain}
+            </div>
+            <h1
+              className='z-40 mx-auto mb-2 block h-auto w-full break-words text-left text-3xl font-bold text-zinc-800 sm:text-4xl'
+              ref={ref}
+            >
+              {title}
+            </h1>
+            {children}
+          </div>
         </article>
       </main>
 
       <Footer />
-    </div>
+    </>
   );
 }
 const BackToExperiments = () => {
   return (
     <Link href={`/#experiments`}>
-      <a className='-ml-1 flex w-1/4 cursor-pointer items-center sm:w-1/6'>
+      <a className='-ml-1 flex w-1/3 cursor-pointer items-center sm:w-1/6'>
         <span className=' inline-block w-full'>
           <span className='group flex items-center'>
             <ChevronLeftIcon
               className='-ml-0.5 h-6 w-6 text-blue-600 sm:group-hover:text-purple-800'
               strokeWidth={2}
             />
-            <div className='-ml-1 truncate text-blue-600 sm:group-hover:text-purple-800 '>
+            <span className=' -ml-1 block truncate text-blue-600 sm:group-hover:text-purple-800 '>
               Experiments
-            </div>
+            </span>
           </span>
         </span>
       </a>
@@ -139,9 +104,9 @@ const BackToExperiments = () => {
   );
 };
 
-const PageTitle = ({ title, inView }: { title: string; inView: any }) => {
+const PageTitleCenter = ({ title, inView }: { title: string; inView: any }) => {
   return (
-    <div className='sm:2/4  -mr-2 inline w-1/2'>
+    <div className='sm:2/4  -mr-2 inline w-1/3'>
       <div className='flex justify-center py-0.5 sm:py-0'>
         <div
           className={`truncate text-center text-lg font-semibold duration-100  ${
@@ -157,7 +122,7 @@ const PageTitle = ({ title, inView }: { title: string; inView: any }) => {
 
 const PageSource = ({ href }: { href: string }) => {
   return (
-    <div className='flex w-1/4  justify-end sm:w-1/6 '>
+    <div className='flex w-1/3  justify-end sm:w-1/6 '>
       <cite className='not-italic'>
         <a
           title='This page source code'
