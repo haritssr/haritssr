3c600b5a0993580ba86d5389ad4f74d6926d50ca
Harits Syah <haritssr@gmail.com>
2022-11-08 14:58:48 +0700

    docs: add project case studies data

---
+++ data/projectCaseStudyData.ts
@@ -0,0 +1,24 @@
+const projectCaseStudyData = [
+	{
+		project_name: 'Haris Lab',
+		about_client: {
+			logo_src: '',
+			brand_name: 'Haris Lab',
+			about: 'Provide online platform for reference, analyze, practice, assesment, and offline tutoring math and physics high school and early college level.',
+			company_name: 'PT Haris Laboratorium Indonesia',
+			phone_number: '+62-8953-3110-3401',
+			website: 'www.harislab.com',
+			office_location: 'jln',
+			client: 'PT Haris Laboratorium Indonesia, via Harits Syah*'
+		},
+		about_project: {
+			my_role: ['','',''],
+			working_periode: '2021 Jun - Nov 2022 (1 year 5 months)',
+			website_status: ['Production'],
+			routes: ['Home','Referensi','Analisis', 'Latihan', 'Bimbel']
+		}
+	}
+]
+
+
+// * www.linkedin.com/in/haritssr
