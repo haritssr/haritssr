a66dc429f970bce30cb50051a977dc3f0de0d54c
Harits Syah <haritssr@gmail.com>
2022-11-08 09:07:32 +0700

    style: some link component

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,45 +1,43 @@
-import { ArrowUpRightIcon } from '@heroicons/react/24/outline';
-
 export default function ExternalLink({
-  name,
-  href,
-  big,
+	name,
+	href,
+	big,
 }: {
-  name: string;
-  href: string;
-  big?: boolean;
+	name: string;
+	href: string;
+	big?: boolean;
 }) {
-  return (
-    <cite className='group not-italic'>
-      {' '}
-      <a
-        title={href}
-        href={href}
-        target='_blank'
-        rel='noopener noreferrer'
-        className='group inline-block w-fit cursor-pointer items-center text-blue-600 hover:underline group-hover:text-blue-700'
-      >
-        <span className='flex items-center'>
-          <span className={`${big ? 'text-lg font-medium' : 'text-base'}`}>{name}</span>
-          {/* External Link Icon */}
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className={` ${
-              big ? 'h-[18px] w-[18px]' : 'h-4 w-4'
-            } ml-1  text-blue-600 group-hover:text-blue-700`}
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            strokeWidth={1.5}
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
-            />
-          </svg>
-        </span>
-      </a>{' '}
-    </cite>
-  );
+	return (
+		<cite className='group not-italic'>
+			{' '}
+			<a
+				title={href}
+				href={href}
+				target='_blank'
+				rel='noopener noreferrer'
+				className='group inline-block w-fit cursor-pointer items-center text-blue-600 hover:underline group-hover:text-blue-700'
+			>
+				<span className='flex items-center'>
+					<span className={`${big ? 'text-lg font-medium' : 'text-base'}`}>{name}</span>
+					{/* External Link Icon */}
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						className={` ${
+							big ? 'h-[18px] w-[18px]' : 'h-4 w-4'
+						} ml-1  text-blue-600 group-hover:text-blue-700 mt-0.5`}
+						fill='none'
+						viewBox='0 0 24 24'
+						stroke='currentColor'
+						strokeWidth={1.5}
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+						/>
+					</svg>
+				</span>
+			</a>{' '}
+		</cite>
+	);
 }
--- components/InternalLink.tsx
+++ components/InternalLink.tsx
@@ -8,11 +8,13 @@ export default function InternalLink({ name, href }: { name: string; href: strin
 			href={href}
 			className='group flex w-fit cursor-pointer items-center justify-start py-0.5 text-[17px] text-blue-600 hover:text-blue-700 hover:underline sm:text-base'
 		>
-			<p>{name}</p>
-			<ChevronRightIcon
-				className='h-3.5 w-3.5 mt-1 text-blue-600 duration-200 group-hover:translate-x-1 group-hover:text-blue-700 group-hover:underline'
-				strokeWidth={2}
-			/>
+			<p className='flex items-center'>
+				{name}
+				<ChevronRightIcon
+					className='h-3.5 w-3.5 mt-1 text-blue-600 duration-200 group-hover:translate-x-1 group-hover:text-blue-700 group-hover:underline'
+					strokeWidth={2}
+				/>
+			</p>
 		</Link>
 	);
 }
