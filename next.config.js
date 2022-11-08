3d7b417cd7c123f254d64740ad895899bfc0ca1b
Harits Syah <haritssr@gmail.com>
2022-11-08 09:07:06 +0700

    refactor: add @next/font

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-  reactStrictMode: false,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    domains: [
-      'vignette.wikia.nocookie.net',
-      'images.unsplash.com',
-      'res.cloudinary.com',
-      'ws-public.interpol.int',
-    ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/experiments',
-        destination: '/#experiments',
-        permanent: false,
-      },
-    ];
-  },
+	reactStrictMode: false,
+	images: {
+		formats: ['image/avif', 'image/webp'],
+		domains: [
+			'vignette.wikia.nocookie.net',
+			'images.unsplash.com',
+			'res.cloudinary.com',
+			'ws-public.interpol.int',
+		],
+	},
+	async redirects() {
+		return [
+			{
+				source: '/experiments',
+				destination: '/#experiments',
+				permanent: false,
+			},
+		];
+	},
+	experimental: {
+		fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
+	},
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,21 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter();
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {/*<React.StrictMode> //disabled for react-dnd preview bug for now */}
-      <DndProvider backend={HTML5Backend}>
-        <IdProvider>
-          <Component {...pageProps} />
-        </IdProvider>
-      </DndProvider>
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			{/*<React.StrictMode> //disabled for react-dnd preview bug for now */}
+			<DndProvider backend={HTML5Backend}>
+				<IdProvider>
+					<Component {...pageProps} className={inter.className} />
+				</IdProvider>
+			</DndProvider>
+		</QueryClientProvider>
+	);
 }
