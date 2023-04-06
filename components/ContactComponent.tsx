dfdf6cc88d7fdef3b335ecb30383ecdbcaff5f4c
Harits Syah <haritssr@gmail.com>
2023-04-06 23:20:18 +0700

    style: delete external link icon at ContactComponent

--- components/ContactComponent.tsx
+++ components/ContactComponent.tsx
@@ -30,21 +30,6 @@ export default function ContactComponent({
 					// placeholder='blur'
 				/>
 				<div className=' text-zinc-800 hover:text-blue-600 group-hover:underline'>{name}</div>
-				{/* External Link Icon */}
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					className='h-4 w-4 text-zinc-400 group-hover:text-blue-600'
-					fill='none'
-					viewBox='0 0 24 24'
-					stroke='currentColor'
-					strokeWidth={1.5}
-				>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
-					/>
-				</svg>
 			</section>
 		</a>
 	);
