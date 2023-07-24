e63ddfa59abf959657afbe348234b3c926f66bd1
Harits Syah <haritssr@gmail.com>
2023-07-24 17:44:55 +0700

    typo "Home" to "Experiments"

--- pages/experiments/index.tsx
+++ pages/experiments/index.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout";
 
 export default function ExperimentsPage() {
   return (
-    <Layout browserTitle="Home" description="Home">
+    <Layout browserTitle="Experiments" description="Home">
       <div className="text-4xl font-bold text-zinc-800 mt-16">Experiments</div>
       <div className="mt-5 mb-8 text-lg text-zinc-500">
         My experiments across JavaScript and React ecosystem stack
