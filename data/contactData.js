ae2518ecc512630d9cb93445e04c77681d992932
Harits Syah <haritssr@gmail.com>
2023-08-03 20:36:36 +0700

    redesign contact section & add about page (#47)

---
+++ data/contactData.js
@@ -0,0 +1,47 @@
+export const aboutMeDataFull = [
+  {
+    section: "Introduction",
+    description: "Brief facts about me",
+    points: [
+      "My name is Harits Syah, I prefer to be called Haris.",
+      "haritssr at haritssr.com mean harits syah rahmatullah.",
+      "I am a Web Software Engineer, Web Designer, and Math-Physics Teacher.",
+      "I am a physics students at UIN Syarif Hidayatullah, Jakarta.",
+      "Founder of Haris Lab, a place to analyze math and physics problem.",
+      "I am based on South Tangerang, Indonesia.",
+    ],
+  },
+  {
+    section: "Contacts",
+    description: "My preferable communication channel",
+    points: [
+      "linkedin.com/in/haritssr",
+      "haritssr@gmail.com",
+      "haritssr@outlook.com",
+    ],
+  },
+  {
+    section: "Social Media",
+    description: "Find my online activity here",
+    points: [
+      "twitter.com/haritssr",
+      "github.com/haritssr",
+      "leetcode.com/haritssr",
+      "haritssr.com",
+    ],
+  },
+  {
+    section: "UX",
+    description: "My preferable source of UX principles",
+    points: [
+      "nngroup.com",
+      "lawsofux.com",
+      "developer.apple.com/design/human-interface-guidelines",
+    ],
+  },
+  {
+    section: "Web Design",
+    description: "I like how their user interface is presented",
+    points: ["apple.com", "linear.app", "vercel.com", "notion.so"],
+  },
+];
