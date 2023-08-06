72dbd821c023a51cb664c47f10e70539a9ca5596
Harits Syah <haritssr@gmail.com>
2023-08-06 22:05:53 +0700

    forget the "key" attr at ExperiementsGrid

--- components/ExperimentsGrid.tsx
+++ components/ExperimentsGrid.tsx
@@ -5,7 +5,7 @@ export default function ExperimentsGrid() {
   return (
     <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
       {ExperimentsData.map((experiment) => {
-        return <ExperimentsCard experiment={experiment} />;
+        return <ExperimentsCard key={experiment.id} experiment={experiment} />;
       })}
     </div>
   );
