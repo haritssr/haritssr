27f6e270aa11b41b52f2eaa553de1ee4f4c17c5e
Harits Syah <haritssr@gmail.com>
2023-08-06 22:52:45 +0700

    unmatched: "PostData" to "postData"

--- utils/posts.js
+++ utils/posts.js
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import { remark } from "remark";
 import html from "remark-html";
 
-const postsDirectory = path.join(process.cwd(), "/data/PostsData");
+const postsDirectory = path.join(process.cwd(), "/data/postsData");
 
 export function getSortedPostsData() {
   // Get file names under /posts as an array
