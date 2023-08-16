bf2c6dd20971e8f3eb2c586702153f7596264d13
Harits Syah <haritssr@gmail.com>
2023-08-17 03:22:05 +0700

    random

---
+++ .contentlayer/.cache/v0.3.1/compiled-contentlayer-config-3TK23UZJ.mjs
@@ -0,0 +1,92 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+var computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath
+  },
+  structuredData: {
+    type: "object",
+    resolve: (doc) => ({
+      "@context": "https://schema.org",
+      "@type": "BlogPosting",
+      headline: doc.title,
+      datePublished: doc.publishedAt,
+      dateModified: doc.publishedAt,
+      description: doc.summary,
+      image: doc.image ? `https://haritssr.vercel.app${doc.image}` : `https://haritssr.vercel.app/og?title=${doc.title}`,
+      url: `https://haritssr.vercel.app/blog/${doc._raw.flattenedPath}`,
+      author: {
+        "@type": "Person",
+        name: "Harits Syah"
+      }
+    })
+  }
+};
+var Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    publishedAt: {
+      type: "string",
+      required: true
+    },
+    summary: {
+      type: "string",
+      required: true
+    },
+    image: {
+      type: "string"
+    }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Blog],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "one-dark-pro",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"];
+          }
+        }
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"]
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Blog,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-3TK23UZJ.mjs.map
