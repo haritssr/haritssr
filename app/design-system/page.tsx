a42eecd84d8c0ba0e44b559601d9b60d4b867e29
Harits Syah <haritssr@gmail.com>
2023-08-18 19:45:58 +0700

    sticky navbar at global navigation route

--- app/design-system/page.tsx
+++ app/design-system/page.tsx
@@ -3,22 +3,26 @@ import React from "react";
 import { PageTitle } from "@/components/SubTitle";
 
 export default function DesignSystem() {
-  return (
-    <>
-      <PageTitle title="Design Systems">
-        This design system used in{" "}
-        <ExternalLink name="haritssr.com" href="haritssr.com" /> and{" "}
-        <ExternalLink name="Haris Lab" href="harislab.com" />
-      </PageTitle>
-      <div className="mt-5 space-y-20">
-        <iframe
-          className="border w-full min-h-screen"
-          width="800"
-          height="450"
-          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FmhfH2JaaCDzRSL71XcSnUw%2FHaris-Lab%3Ftype%3Ddesign%26node-id%3D1416%253A236%26mode%3Ddesign%26t%3DwxLQxcZHLYNHFvrj-1"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </>
-  );
+	return (
+		<PageTitle
+			title="Design Systems"
+			description={
+				<>
+					This design system used in{" "}
+					<ExternalLink name="haritssr.com" href="haritssr.com" /> and{" "}
+					<ExternalLink name="Haris Lab" href="harislab.com" />
+				</>
+			}
+		>
+			<div className="mt-5 space-y-20">
+				<iframe
+					className="border w-full min-h-screen"
+					width="800"
+					height="450"
+					src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FmhfH2JaaCDzRSL71XcSnUw%2FHaris-Lab%3Ftype%3Ddesign%26node-id%3D1416%253A236%26mode%3Ddesign%26t%3DwxLQxcZHLYNHFvrj-1"
+					allowFullScreen
+				></iframe>
+			</div>
+		</PageTitle>
+	);
 }
--- pages/experiments/index.tsx
+++ pages/experiments/index.tsx
@@ -4,18 +4,23 @@ import { PageTitle } from "@/components/SubTitle";
 import { ExperimentsData } from "data/ExperimentsData";
 
 export default function ExperimentsPage() {
+	let totalExperiment = 0;
+	for (let i = 0; i < ExperimentsData.length; i++) {
+		totalExperiment += ExperimentsData[i].links.length;
+	}
 
-  let totalExperiment = 0
-  for(let i = 0; i < ExperimentsData.length; i++){
-    totalExperiment += ExperimentsData[i].links.length
-  }
-
-  return (
-    <Layout browserTitle="Experiments" description="Home">
-      <PageTitle title="Experiments">
-      {`${totalExperiment} unique individual (per page) experiments across Javascript-React ecosystem stack`}
-      </PageTitle>
-      <ExperimentsGrid />
-    </Layout>
-  );
+	return (
+		<Layout browserTitle="Experiments" description="Home">
+			<PageTitle
+				title="Experiments"
+				description={
+					<>
+						{`${totalExperiment} unique individual (per page) experiments across Javascript-React ecosystem stack`}
+					</>
+				}
+			>
+				<ExperimentsGrid />
+			</PageTitle>
+		</Layout>
+	);
 }
