d2e6a47c7ae173b0d8c16d61be87862be5b3c530
Harits Syah <haritssr@gmail.com>
2023-08-18 18:52:11 +0700

    add description of total experiments

--- pages/experiments/index.tsx
+++ pages/experiments/index.tsx
@@ -1,12 +1,19 @@
 import ExperimentsGrid from "@/components/ExperimentsGrid";
 import Layout from "@/components/Layout";
 import { PageTitle } from "@/components/SubTitle";
+import { ExperimentsData } from "data/ExperimentsData";
 
 export default function ExperimentsPage() {
+
+  let totalExperiment = 0
+  for(let i = 0; i < ExperimentsData.length; i++){
+    totalExperiment += ExperimentsData[i].links.length
+  }
+
   return (
     <Layout browserTitle="Experiments" description="Home">
       <PageTitle title="Experiments">
-        My experiments across JavaScript and React ecosystem stack
+      {`${totalExperiment} unique individual (per page) experiments across Javascript-React ecosystem stack`}
       </PageTitle>
       <ExperimentsGrid />
     </Layout>
