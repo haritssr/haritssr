b350c0cd109c39d98ac4bc6947fe14103898dac1
Harits Syah <haritssr@gmail.com>
2023-08-18 18:56:35 +0700

    youtube thumbnail not responsive at tailwindcss

--- pages/experiments/tailwind-css/youtube-thumbnail.tsx
+++ pages/experiments/tailwind-css/youtube-thumbnail.tsx
@@ -6,13 +6,13 @@ export default function YoutubeThumbnail() {
     <LayoutToExperiments title='YouTube thumbnail' domain='Tailwind CSS'>
       <SubTitle>Youtube Thumbnail example</SubTitle>
 
-      <div className='relative h-[360px] w-[640px] overflow-hidden  rounded-md border border-purple-300 bg-gradient-to-br from-zinc-50 to-purple-100 shadow-md shadow-purple-100'>
+      <div className='relative h-[360px] w-full overflow-hidden  rounded-md border border-purple-300 bg-gradient-to-br from-zinc-50 to-purple-100 shadow-md shadow-purple-100'>
         <div>
           <Play />
         </div>
         <div aria-hidden='true' className='w-[512px]h-[512px] absolute bg-purple-400'></div>
 
-        <div className='relative flex h-full w-2/3 flex-col justify-end space-y-4 p-8'>
+        <div className='relative flex h-full flex-col justify-end space-y-4 p-8'>
           <p className='text-xs font-semibold uppercase text-purple-600'>How we&apos;d build it</p>
           <h1 className='text-gray 900 text-4xl font-semibold'>
             Youtube Thumbail with Tailwind CSS
