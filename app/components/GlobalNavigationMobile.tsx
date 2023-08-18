4735e51bd695bb2981ded95ab04fc6376babc499
Harits Syah <haritssr@gmail.com>
2023-08-18 19:46:17 +0700

    increase icon size GlobalNavigationMobile

--- app/components/GlobalNavigationMobile.tsx
+++ app/components/GlobalNavigationMobile.tsx
@@ -6,51 +6,51 @@ import React from "react";
 import { TitleAndPathData } from "data/TitleAndPathData";
 
 export default function GlobalNavigationMobile() {
-  return (
-    <div className="sticky bottom-0 block  w-full border-t border-zinc-300 bg-white/70 saturate-150 backdrop-blur-lg sm:hidden">
-      <div className="flex w-full items-center ">
-        {TitleAndPathData.map(({ title, path }) => (
-          <IconWrapper key={title} to={`/${title}`} path={path} />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="sticky bottom-0 block  w-full border-t border-zinc-300 bg-white/70 saturate-150 backdrop-blur-lg sm:hidden">
+			<div className="flex w-full items-center ">
+				{TitleAndPathData.map(({ title, path }) => (
+					<IconWrapper key={title} to={`/${title}`} path={path} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 const IconWrapper = ({ to, path }: { to: string; path: React.ReactNode }) => {
-  const router = usePathname();
-  let color: string;
-  if (router === to) {
-    color = "text-blue-600";
-  } else if (router === "/" && to === "/home") {
-    color = "text-blue-600/90";
-  } else {
-    color = "text-zinc-500";
-  }
-  function capitalizeFirstLetter(to: string) {
-    return to.substring(1).charAt(0).toUpperCase() + to.slice(2);
-  }
+	const router = usePathname();
+	let color: string;
+	if (router === to) {
+		color = "text-blue-600";
+	} else if (router === "/" && to === "/home") {
+		color = "text-blue-600/90";
+	} else {
+		color = "text-zinc-500";
+	}
+	function capitalizeFirstLetter(to: string) {
+		return to.substring(1).charAt(0).toUpperCase() + to.slice(2);
+	}
 
-  return (
-    <Link
-      passHref
-      href={`${to === "/home" ? "/" : to}`}
-      className="block w-1/5 active:scale-95"
-    >
-      <div className="flex flex-col items-center justify-center py-[5px]">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={`h-6 w-6 ${color}`}
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          {path}
-        </svg>
+	return (
+		<Link
+			passHref
+			href={`${to === "/home" ? "/" : to}`}
+			className="block w-1/5 active:scale-95"
+		>
+			<div className="flex flex-col items-center justify-center py-[5px]">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className={`h-7 w-7 ${color}`}
+					viewBox="0 0 24 24"
+					fill="currentColor"
+				>
+					{path}
+				</svg>
 
-        <div className={`text-[11px] leading-[15px] -mt-[1px] ${color}`}>
-          {capitalizeFirstLetter(to)}
-        </div>
-      </div>
-    </Link>
-  );
+				<div className={`text-[11px] leading-[15px] -mt-[1px] ${color}`}>
+					{capitalizeFirstLetter(to)}
+				</div>
+			</div>
+		</Link>
+	);
 };
