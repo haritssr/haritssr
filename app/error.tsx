2af2abaddacc4ceca0a15a4c6d95d0d82f996552
Harits Syah <haritssr@gmail.com>
2023-08-19 14:13:18 +0700

    styling error page

--- app/error.tsx
+++ app/error.tsx
@@ -3,20 +3,28 @@
 import { useEffect } from "react";
 
 export default function Error({
-  error,
-  reset,
+	error,
+	reset,
 }: {
-  error: Error;
-  reset: () => void;
+	error: Error;
+	reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
+	useEffect(() => {
+		// Log the error to an error reporting service
+		console.error(error);
+	}, [error]);
 
-  return (
-    <div>
-      <p>Something error... please refresh the site.</p>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<div className="space-y-10">
+				<div>Something error...</div>
+				<button
+					onClick={() => reset()}
+					className="px-3 py-2 rounded-md bg-zinc-800 text-white hover:bg-zinc-600"
+				>
+					Refresh page
+				</button>
+			</div>
+		</div>
+	);
 }
