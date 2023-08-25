543604935cf7836a73578183fdf99f0e3d768651
Harits Syah <haritssr@gmail.com>
2023-08-25 21:08:56 +0700

    fix: link not working, now working

--- data/AboutData.js
+++ data/AboutData.js
@@ -1,61 +1,58 @@
 export const AboutData = [
-  {
-    section: "Introduction",
-    description: "Brief facts about me",
-    points: [
-      "My name is Harits Syah, I prefer to be called Haris.",
-      "haritssr at haritssr.com mean harits syah rahmatullah.",
-      "I am a Web Software Engineer, Web Designer, and Math-Physics Teacher.",
-      "I am a physics students at UIN Syarif Hidayatullah, Jakarta.",
-      "Founder of Haris Lab, a place to analyze math and physics problem.",
-      "I am based on South Tangerang, Indonesia.",
-    ],
-  },
-  {
-    section: "Contacts",
-    description: "My preferable communication channel",
-    points: [
-      "https://www.linkedin.com/in/haritssr",
-      "haritssr@gmail.com",
-      "haritssr@outlook.com",
-    ],
-  },
-  {
-    section: "Social Media",
-    description: "Find my online activity here",
-    points: [
-      "https://www.twitter.com/haritssr",
-      "https://www.github.com/haritssr",
-      "https://www.leetcode.com/haritssr",
-      "https://www.haritssr.com",
-    ],
-  },
-  {
-    section: "UX",
-    description: "My preferable source of UX principles",
-    points: [
-      "https://www.nngroup.com",
-      "https://www.lawsofux.com",
-      "https://www.developer.apple.com/design/human-interface-guidelines",
-    ],
-  },
-  {
-    section: "Web Design",
-    description:
-      "I like how their user interface is presented and inspired to be like that",
-    points: [
-      "https://www.apple.com",
-      "https://www.linear.app",
-      "https://www.vercel.com",
-      "https://www.notion.so",
-    ],
-  },
-  {
-    section: "Podcast",
-    description: "Favorite one that I frequently listen",
-    points: [
-      "https://www.frontendfirst.fm",
-      "https://www.hackersincorporated.com",
-    ],
-  },
+	{
+		section: "Introduction",
+		description: "Brief facts about me",
+		points: [
+			"My name is Harits Syah, I prefer to be called Haris.",
+			"haritssr at haritssr.com mean harits syah rahmatullah.",
+			"I am a Web Software Engineer, Web Designer, and Math-Physics Teacher.",
+			"I am a physics students at UIN Syarif Hidayatullah, Jakarta.",
+			"Founder of Haris Lab, a place to analyze math and physics problem.",
+			"I am based on South Tangerang, Indonesia.",
+		],
+	},
+	{
+		section: "Contacts",
+		description: "My preferable communication channel",
+		points: [
+			"https://www.linkedin.com/in/haritssr",
+			"haritssr@gmail.com",
+			"haritssr@outlook.com",
+		],
+	},
+	{
+		section: "Social Media",
+		description: "Find my online activity here",
+		points: [
+			"https://www.twitter.com/haritssr",
+			"https://www.github.com/haritssr",
+			"https://www.leetcode.com/haritssr",
+			"https://www.haritssr.com",
+		],
+	},
+	{
+		section: "UX",
+		description: "My preferable source of UX principles",
+		points: [
+			"https://www.nngroup.com",
+			"https://www.lawsofux.com",
+			"https://developer.apple.com/design/human-interface-guidelines",
+		],
+	},
+	{
+		section: "Web Design",
+		description:
+			"I like how their user interface is presented and inspired to be like that",
+		points: [
+			"https://www.apple.com",
+			"https://www.linear.app",
+			"https://www.vercel.com",
+			"https://www.notion.so",
+		],
+	},
+	{
+		section: "Podcast",
+		description: "Favorite one that I frequently listen",
+		points: ["https://frontendfirst.fm", "https://hackersincorporated.com"],
+	},
 ];
