6af95ad1128ea0ff21bfa5be6e40a0ecc2bd1310
Harits Syah <haritssr@gmail.com>
2023-08-27 13:06:50 +0700

    componentize global link

--- components/GlobalNavigation.tsx
+++ components/GlobalNavigation.tsx
@@ -1,15 +1,9 @@
-"use client";
-
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { GitHubIcon } from "./Icons";
 import Image from "next/image";
-import {
-	MagnifyingGlassIcon,
-	MoonIcon,
-	SunIcon,
-} from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import Mode from "app/components/Mode";
+import Destination from "app/components/Destination";
 
 export default function GlobalNavigation() {
 	return (
@@ -40,14 +34,16 @@ export default function GlobalNavigation() {
 				</div>
 
 				<div className="flex items-center space-x-3">
-					{/* Search */}
-					<MagnifyingGlassIcon className="h-5 w-5 text-zinc-800 hover:text-zinc-400 cursor-pointer" />
-					{/* Light/Dark Mode Toggle */}
-					<Mode />
+					<a title="Search: Coming soon">
+						<MagnifyingGlassIcon className="h-5 w-5 text-zinc-800 hover:text-zinc-400 cursor-pointer" />
+					</a>
+					<a title="Dark Mode: Coming soon">
+						<Mode />
+					</a>
 					{/* Source to GitHub */}
 					<a
 						className="flex items-center space-x-2 group"
-						title="Whole site source code"
+						title="Whole Site Source Code"
 						href="https://www.github.com/haritssr/haritssr"
 						target="_blank"
 						rel="noopener noreferrer"
@@ -68,34 +64,3 @@ const links = [
 	"pure",
 	"about",
 ];
-
-function Destination({ link }: { link: string }) {
-	const pathname = usePathname();
-
-	const CurrentPageBaseRoute = pathname?.split("/")[1];
-
-	function capitalizeFirstLetter(to: string) {
-		return to.substring(0).charAt(0).toUpperCase() + to.slice(1);
-	}
-
-	let color: string;
-
-	if (pathname === `/${link}`) {
-		color = "text-blue-700";
-	} else if (CurrentPageBaseRoute === link) {
-		color = "text-blue-700";
-	} else {
-		color = "text-zinc-800 hover:text-zinc-400";
-	}
-
-	return (
-		<li>
-			<Link
-				className={`${color}`}
-				href={`${link === "pure" ? "/pure" : `/${link}`}`}
-			>
-				{capitalizeFirstLetter(link)}
-			</Link>
-		</li>
-	);
-}
