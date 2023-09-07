a8dbec50da6b9adf98f5e740e45390f9e9710d89
Harits Syah <haritssr@gmail.com>
2023-09-07 12:51:23 +0700

    enhance design of <PopoverContact/>

--- app/components/PopoverContact.tsx
+++ app/components/PopoverContact.tsx
@@ -9,7 +9,7 @@ export default function PopoverContact() {
 		<div className="flex justify-center">
 			<Popover.Root>
 				<Popover.Trigger>
-					<div className="space-x-1 flex items-center rounded-md py-0.5 pl-1.5 pr-1  hover:bg-zinc-50 active:ring-1 active:ring-offset-1 active:ring-zinc-500 select-none rounded-md ring-1 ring-zinc-950/20 focus:outline-none focus:ring-zinc-800 shadow">
+					<div className="space-x-1 flex items-center py-0.5 pl-1.5 pr-1  hover:bg-zinc-50 active:ring-1 active:ring-offset-1 active:ring-zinc-500 select-none rounded-md ring-1 ring-zinc-950/20 focus:outline-none focus:ring-zinc-800 shadow ">
 						<div className="text-zinc-600 text-sm">@haritssr</div>
 						<svg fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-4 h-4 text-action">
 							<path
@@ -21,38 +21,27 @@ export default function PopoverContact() {
 					</div>
 				</Popover.Trigger>
 				<Popover.Content
-					className="rounded-lg border border-zinc-300 bg-white w-fit shadow-xl max-w-sm text-zinc-800 p-3 z-50"
+					className="rounded-lg border overflow-hidden border-zinc-300 bg-white w-fit shadow-2xl max-w-sm text-zinc-800 z-50"
 					sideOffset={10}
 					side="bottom"
 				>
-					<ul className="block space-y-2">
+					<ul className="block divide-y divide-zinc-300">
 						{ContactData.points.map((each) => (
-							<li key={each.link} className="text-zinc-700 flex space-x-2 items-center">
+							<li
+								key={each.link}
+								className="text-zinc-600 hover:text-zinc-800 cursor-pointer flex space-x-2 px-3 py-1.5 items-center hover:bg-zinc-50"
+							>
 								<Image alt={each.link} src={each.icon} height={20} width={20} className="h-[18px] w-[18px]" />
 								{each.link.startsWith("https://www.") ? (
-									<a
-										title={each.link}
-										className="hover:underline hover:text-zinc-800"
-										target="_blank"
-										rel="noreferrer noopener"
-										href={each.link}
-									>
+									<a title={each.link} target="_blank" rel="noreferrer noopener" href={each.link}>
 										{each.link.slice(12)}
 									</a>
 								) : each.link.startsWith("https://") ? (
-									<a
-										title={each.link}
-										className="hover:underline hover:text-zinc-800"
-										target="_blank"
-										rel="noreferrer noopener"
-										href={each.link}
-									>
+									<a title={each.link} target="_blank" rel="noreferrer noopener" href={each.link}>
 										{each.link.slice(8)}
 									</a>
 								) : each.link.includes("@") ? (
-									<a className="hover:underline hover:text-zinc-800" href={`mailto:${each.link}`}>
-										{each.link}
-									</a>
+									<a href={`mailto:${each.link}`}>{each.link}</a>
 								) : (
 									each.link
 								)}
