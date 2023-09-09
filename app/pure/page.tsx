7913c638374c3dd4ec69ac53bd40eb2784369792
Harits Syah <haritssr@gmail.com>
2023-09-09 07:47:14 +0700

    add button with only icon

--- app/pure/page.tsx
+++ app/pure/page.tsx
@@ -334,6 +334,24 @@ export default function DesignSystem() {
 						<span>Bookmark</span>
 					</button>
 				</Box>
+				<Box title="Button: Only Icon">
+					<button className="p-1.5 text-zinc-800 font-medium hover:bg-zinc-50 active:ring-1 active:ring-offset-1 active:ring-zinc-500 select-none rounded-md ring-1 ring-zinc-950/20 focus:outline-none focus:ring-zinc-800 shadow flex items-center space-x-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={2}
+							stroke="currentColor"
+							className="w-5 h-5 text-zinc-800"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+							/>
+						</svg>
+					</button>
+				</Box>
 				<Box title="Button: Danger">
 					<button className="px-3 py-1 text-red-600 font-medium hover:bg-zinc-50 active:ring-1 active:ring-offset-1 active:ring-red-500 select-none rounded-md ring-1 ring-zinc-950/20 focus:outline-none focus:ring-zinc-600 shadow">
 						Button
