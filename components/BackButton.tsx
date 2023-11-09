0e76116a4400b156e95bbc962ca4e8ebd5cce273
Harits Syah <haritssr@gmail.com>
2023-11-09 18:10:44 +0700

    add hover:underline at every link component

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -6,13 +6,8 @@ export default function BackButton({ name, href }: { name: string; href: string 
 		<Link passHref href={href} className="block">
 			<span className=" inline-block w-full">
 				<span className="group flex items-center">
-					<ChevronLeftIcon
-						className="-ml-2 h-5 w-5 pt-[1px] text-action sm:group-hover:text-[#2563eb]/80"
-						strokeWidth={2}
-					/>
-					<span className="-ml-0.5 block truncate text-action sm:group-hover:text-[#2563eb]/80 sm:active:underline">
-						{name}
-					</span>
+					<ChevronLeftIcon className="-ml-2 h-5 w-5 pt-[1px] text-action sm:group-hover:text-[#2563eb]/80" strokeWidth={2} />
+					<span className="-ml-0.5 block truncate text-action sm:group-hover:text-[#2563eb]/80 sm:hover:underline">{name}</span>
 				</span>
 			</span>
 		</Link>
--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -7,26 +7,20 @@ export default function ExternalLink({ name, href, big }: { name: string; href: 
 				href={href}
 				target="_blank"
 				rel="noopener noreferrer"
-				className="group inline-block w-fit cursor-pointer items-center text-action sm:active:underline group-hover:text-[#2563eb]/80"
+				className="group inline-block w-fit cursor-pointer items-center text-action sm:hover:underline group-hover:text-[#2563eb]/80"
 			>
 				<span className="flex items-center">
 					<span className={`${big ? "text-lg font-medium" : "text-base"}`}>{name}</span>
 					{/* External Link Icon */}
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						className={` ${
-							big ? "h-[18px] w-[18px]" : "h-4 w-4"
-						} ml-1  text-action group-hover:text-[#2563eb]/80 mt-0.5`}
+						className={` ${big ? "h-[18px] w-[18px]" : "h-4 w-4"} ml-1  text-action group-hover:text-[#2563eb]/80 mt-0.5`}
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke="currentColor"
 						strokeWidth={1.7}
 					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-						/>
+						<path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
 					</svg>
 				</span>
 			</a>{" "}
