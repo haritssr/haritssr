20a18df9f47185ab04e8d5381092c6590d4dfd8b
Harits Syah <haritssr@gmail.com>
2024-01-30 00:59:33 +0700

    adjustment of description size for main routes

--- components/SubTitle.tsx
+++ components/SubTitle.tsx
@@ -10,7 +10,7 @@ export function PageTitle({ title, description }: { title: string; description: 
 	return (
 		<div>
 			<div className="text-3xl sm:text-4xl font-bold text-zinc-800 mt-10 sm:mt-16">{title}</div>
-			<div className="mt-4 mb-10 break-words text-zinc-700 text-base sm:text-lg">{description}</div>
+			<div className="mt-4 mb-10 break-words text-zinc-700 text-lg sm:text-xl">{description}</div>
 		</div>
 	);
 }
