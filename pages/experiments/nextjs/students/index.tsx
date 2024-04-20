a037e651e97f68b987e483268e121a3b7fa9e442
Harits Syah <haritssr@gmail.com>
2024-04-20 16:24:39 +0700

    make students page flex-col

--- pages/experiments/nextjs/students/index.tsx
+++ pages/experiments/nextjs/students/index.tsx
@@ -1,26 +1,26 @@
-import LayoutToExperiments from '@/components/LayoutToExperiments';
-import SubTitle from '@/components/SubTitle';
-import InternalLink from '@/components/InternalLink';
-import { GetStaticProps } from 'next';
+import LayoutToExperiments from "@/components/LayoutToExperiments";
+import SubTitle from "@/components/SubTitle";
+import InternalLink from "@/components/InternalLink";
+import { GetStaticProps } from "next";
 
 export default function Students({ students }) {
-  return (
-    <LayoutToExperiments title='Students' domain='Nextjs'>
-      <SubTitle>A list of students come from JSONPlaceholder API</SubTitle>
-      <div className='mt-2 space-y-3'>
-        {students.map(a => (
-          <InternalLink key={a.id} name={a.name} href={`/experiments/nextjs/students/${a.id}`} />
-        ))}
-      </div>
-    </LayoutToExperiments>
-  );
+	return (
+		<LayoutToExperiments title="Students" domain="Nextjs">
+			<SubTitle>A list of students come from JSONPlaceholder API</SubTitle>
+			<div className="space-y-3 flex flex-col">
+				{students.map((a) => (
+					<InternalLink key={a.id} name={a.name} href={`/experiments/nextjs/students/${a.id}`} />
+				))}
+			</div>
+		</LayoutToExperiments>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
 
-  return {
-    props: { students: data },
-  };
+	return {
+		props: { students: data },
+	};
 };
