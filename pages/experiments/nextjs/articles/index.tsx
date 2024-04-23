93cebdefebda7b76b7856c6b8277dcaa5816188e
Harits Syah <haritssr@gmail.com>
2024-04-23 13:41:56 +0700

    delete article content

--- pages/experiments/nextjs/articles/index.tsx
+++ pages/experiments/nextjs/articles/index.tsx
@@ -8,9 +8,7 @@ import { ReactChild, ReactFragment, ReactPortal } from "react";
 
 export async function getStaticProps() {
 	// https://stackoverflow.com/questions/52842039/how-to-limit-the-amount-of-data-returned-from-a-json-file-using-fetch
-	const posts = await fetch(
-		"https://jsonplaceholder.typicode.com/posts/?_limit=20"
-	).then((post) => post.json());
+	const posts = await fetch("https://jsonplaceholder.typicode.com/posts/?_limit=20").then((post) => post.json());
 	return {
 		props: {
 			posts,
@@ -23,23 +21,13 @@ export default function Articles({ posts }) {
 		<LayoutToExperiments title="Articles " domain="Nextjs">
 			<SubTitle>
 				getStaticProps + Dynamic Pages. This data generated from{" "}
-				<ExternalLink
-					name="json placeholder"
-					href="https://jsonplaceholder.typicode.com/posts"
-				/>{" "}
-				using
-				<ExternalLink
-					name="getStaticProps"
-					href="https://nextjs.org/docs/basic-features/data-fetching/get-static-props"
-				/>
+				<ExternalLink name="json placeholder" href="https://jsonplaceholder.typicode.com/posts" /> using
+				<ExternalLink name="getStaticProps" href="https://nextjs.org/docs/basic-features/data-fetching/get-static-props" />
 				and populated to new page for each article using
-				<ExternalLink
-					name="getStaticPaths"
-					href="https://nextjs.org/docs/basic-features/data-fetching/get-static-paths"
-				/>
+				<ExternalLink name="getStaticPaths" href="https://nextjs.org/docs/basic-features/data-fetching/get-static-paths" />
 			</SubTitle>
 
-			<div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 ">
+			<div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 sm:grid-cols-3">
 				{posts.map(
 					(post: {
 						id: number;
@@ -53,10 +41,8 @@ export default function Articles({ posts }) {
 							className="rounded-md border border-zinc-300 bg-zinc-50 p-4 duration-200 ease-out hover:cursor-pointer hover:bg-white "
 						>
 							<div className="text-action ">Article {post.id}</div>
-							<div className="text-xl font-semibold text-gray-800 ">
-								{post.title}
-							</div>
-							<div className="text-gray-600 ">{post.body}</div>
+							<div className="text-xl font-semibold text-gray-800">{post.title}</div>
+							{/* <p className="text-gray-600 text-ellipsis">{post.body}</p> */}
 						</Link>
 					)
 				)}
