17909e162e0bd20a6b6f7dc84fc81635a4d002dd
Harits Syah <haritssr@gmail.com>
2024-04-23 14:08:41 +0700

    fix breadcrumbs bug, adding description, better function name

--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -3,33 +3,40 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Breadcrumbs() {
-	const pathname = usePathname(); //         /experiments/react/usestate-form
-	const routes = pathname?.split("/"); // ['experiments','react','usestate-form']
+	// e.g /experiments/react/usestate-form
+	const pathname = usePathname();
+	// ['experiments','react','usestate-form']
+	const routes = pathname?.split("/");
 
-	function getUpto(routes: string[], route: string) {
-		const indexOfCurrentRoute = routes?.indexOf(route); // route = 'react', 1
-		const goto = routes.slice(0, indexOfCurrentRoute + 1).join("/"); // /experiments/react
+	function getLinkFromRoutesUpToRoute(routes: string[], route: string) {
+		// route = 'react', so the value is 1
+		const indexOfCurrentRoute = routes?.indexOf(route);
+		// /experiments/react
+		const goto = routes.slice(0, indexOfCurrentRoute + 1).join("/");
 		return goto;
 	}
 
 	return (
 		<div className="sm:px-5 py-1.5 bg-white dark:bg-zinc-800 sm:border-b sm:dark:border-zinc-700 sticky top-[45px] sm:-mx-5">
-			<Link className="hover:underline text-zinc-400 sm:text-lg hover:text-zinc-800 dark:hover:text-zinc-200" href="/">
+			<Link className="hover:underline text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200" href="/">
 				home
 			</Link>
 			{routes?.map((route, i, array) => {
+				// if the i is the last element of an array or the last route or the current route/page
 				return i === array.length - 1 ? (
-					<span key={route}>
-						<span className="text-zinc-400">{route}</span>
+					// this is the current route/page
+					<span key={route} className="text-zinc-800">
+						{route}
 					</span>
 				) : (
 					<span key={route}>
-						<Link className="hover:underline text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200" href={`${getUpto(routes, route)}`}>
+						<Link
+							className="hover:underline text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200"
+							href={`${getLinkFromRoutesUpToRoute(routes, route)}`}
+						>
 							{route}
 						</Link>
 						&nbsp;&nbsp;<span className="text-zinc-400">/</span>&nbsp;&nbsp;
-						{/* I just write something here */}
-						<Link href={routes[0]}>{route}</Link>
 					</span>
 				);
 			})}
