6020441f089a9eba54c9522afe52b02ed4f7db4d
Harits Syah <haritssr@gmail.com>
2024-04-28 11:53:45 +0700

    typo image host name, interpol

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "ws-public.interpol.intt",
+				hostname: "ws-public.interpol.int",
 				port: "",
 				pathname: "/**",
 			},
