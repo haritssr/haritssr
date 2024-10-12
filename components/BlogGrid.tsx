861c8642c06a61097538cbd52a07a84a0ffe72c9
Harits Syah <haritssr@gmail.com>
2024-10-13 02:16:57 +0700

    Better typography <BlogGrid/>

--- components/BlogGrid.tsx
+++ components/BlogGrid.tsx
@@ -15,17 +15,17 @@ export default function BlogGrid() {
 					className="rounded-lg border border-zinc-300 px-3 py-2 hover:bg-zinc-50 flex bg-white items-start justify-between  group"
 				>
 					<>
-						<div className="w-full text-zinc-400">
+						<div className="w-full text-zinc-500">
 							<p className="text-zinc-800">{post.title}</p>
-							<p className="inline text-[14px] font-light">
+							<p className="inline text-[15px] font-light">
 								{post.publishedAt}
 							</p>
 							.&nbsp;
-							<p className="inline text-[14px] font-light">
+							<p className="inline text-[15px] font-light">
 								{post.structuredData.wordCount} Words
 							</p>
 							, &nbsp;
-							<p className="inline text-[14px] font-light">
+							<p className="inline text-[15px] font-light">
 								{Math.ceil(post.structuredData.wordCount / 200)} min read.
 							</p>
 						</div>
