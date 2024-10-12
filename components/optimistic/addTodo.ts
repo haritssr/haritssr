0442da2097898ad4a4b3a3ca8330bbc7b5dc1d24
Harits Syah <haritssr@gmail.com>
2024-10-13 01:08:30 +0700

    Delete unused files

--- components/optimistic/addTodo.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-type Todo = {
-	todo: string;
-};
-export async function addTodo(newTodo: string): Promise<Todo> {
-	await new Promise((resolve) => setTimeout(resolve, 3000));
-	return { todo: `${newTodo} test` };
-}
