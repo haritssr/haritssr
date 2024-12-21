cd6d5945ee604da345436e75669922803dc9c41f
Harits Syah <haritssr@gmail.com>
2024-12-21 10:32:11 +0700

    add list of new experiments in app/new-experiments

--- app/new-experiments/page.tsx
+++ app/new-experiments/page.tsx
@@ -1,14 +1,18 @@
 import InternalLink from "@/components/InternalLink";
 
+const newExperimentsPages = ["inline maki", "countdown mabes", "form react 19"];
+
 export default function NewExperimentsPage() {
 	return (
 		<div className="my-10 flex flex-col space-y-2">
-			<InternalLink href="/new-experiments/inline-maki">
-				Inline MAKI
-			</InternalLink>
-			<InternalLink href="/new-experiments/countdown-mabes">
-				Countdown Mabes
-			</InternalLink>
+			{newExperimentsPages.map((page) => (
+				<InternalLink
+					key={page}
+					href={`/new-experiments/${page.split(" ").join("-")}`}
+				>
+					Inline MAKI
+				</InternalLink>
+			))}
 		</div>
 	);
 }
