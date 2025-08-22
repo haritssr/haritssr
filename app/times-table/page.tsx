d22e9f359d6c04ce1993a5fa732ecd399d63df6d
Harits Syah <haritssr@gmail.com>
2025-08-22 21:52:05 +0700

    typo

--- app/times-table/page.tsx
+++ app/times-table/page.tsx
@@ -3,6 +3,6 @@ import { PageTitle } from "@/components/SubTitle";
 export default function TimesTable(){
   return <PageTitle
   title="Times Table"
-  description="Self-corrected Mechanism 10x10 times table with statistics."
+  description="Self-corrected 10x10 times table with statistics."
 />
 }
