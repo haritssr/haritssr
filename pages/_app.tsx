abb9014a95fad1f265d31a89aed2aee7b6dc1eb2
Harits Syah <haritssr@gmail.com>
2025-11-15 07:07:39 +0700

    delete unnecesary tailwindcss import in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
 const queryClient = new QueryClient();
