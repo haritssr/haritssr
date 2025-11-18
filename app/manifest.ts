d8ada300e930853a487841d5ed5afb61d5af031c
Harits Syah <haritssr@gmail.com>
2025-11-18 12:55:30 +0700

    editing manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,17 +10,17 @@ export default function manifest(): MetadataRoute.Manifest {
 		display: "standalone",
 		background_color: "#ffffff",
 		theme_color: "#000000",
-		icons: [
-			{
-				src: "/icon-192x192.png",
-				sizes: "192x192",
-				type: "image/svg",
-			},
-			{
-				src: "/icon-512x512.png",
-				sizes: "512x512",
-				type: "image/svg",
-			},
-		],
+	// icons: [
+	// 	{
+	// 		src: "/static/favicons/android-chrome-192x192.png",
+	// 		sizes: "192x192",
+	// 		type: "image/png",
+	// 	},
+	// 	{
+	// 		src: "/static/favicons/android-chrome-512x512.png",
+	// 		sizes: "512x512",
+	// 		type: "image/png",
+	// 	},
+	// ],
 	};
 }
