e59535e05eb6151f6b033166a9b177c9411248ad
Harits Syah <haritssr@gmail.com>
2025-11-30 08:48:29 +0700

    app router page should be RSC, move CC into separate file

---
+++ app/times-table/IncrementButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { parseAsInteger, useQueryState } from "nuqs";
+
+export function IncrementButton() {
+	const [count, setCount] = useQueryState(
+		"count",
+		parseAsInteger.withDefault(0),
+	);
+
+	const [search, setSearch] = useQueryState("search", { defaultValue: " " });
+	return (
+		<>
+			<button
+				className="block my-5 border rounded-2xl corner-squircle px-4 py-1.5 hover:bg-zinc-50 active:bg-zinc-100 border-zinc-300 cursor-pointer active:border-zinc-400 active:translate-y-0.5 text-zinc-700 select-none"
+				type="button"
+				onClick={() => setCount((c) => c + 1)}
+			>
+				Count: {count}
+			</button>
+			<input
+				type="search"
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+		</>
+	);
+}
