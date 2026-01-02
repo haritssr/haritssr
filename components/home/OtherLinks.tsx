6712bf1c4d33cba8006ede35e5d86ed08e3ff9a8
Harits Syah <haritssr@gmail.com>
2026-01-03 02:30:59 +0700

    chore: Delete /technology

--- components/home/OtherLinks.tsx
+++ components/home/OtherLinks.tsx
@@ -1,15 +1,12 @@
 import InternalLinkWithoutArrow from "../InternalLinkWithoutArrow";
 
-export default function Others() {
+export default function OtherLinks() {
 	return (
 		<section>
 			<div className=" mb-3 select-none text-2xl font-semibold text-zinc-800">
 				Others
 			</div>
 			<ul className="list-inside space-y-1">
-				<li>
-					<InternalLinkWithoutArrow href="/technology" name="Technology" />
-				</li>
 				<li>
 					<InternalLinkWithoutArrow href="/pure" name="Pure Design System" />
 				</li>
