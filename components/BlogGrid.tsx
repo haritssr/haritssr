6d5911c52d35cf0153b7e27048a09a1e231eab2a
Harits Syah <haritssr@gmail.com>
2026-01-17 09:51:56 +0700

    refactor(BlogGrid.tsx): simplify link view

--- components/BlogGrid.tsx
+++ components/BlogGrid.tsx
@@ -1,36 +1,15 @@
 "use client";
 
 import { allBlogs } from ".contentlayer/generated";
-import Link from "next/link";
+import InternalLink from "./InternalLink";
 
 export default function BlogGrid() {
   return (
-    <div className="grid gap-5 lg:grid-cols-2">
+    <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {allBlogs.map((post) => (
-        <Link
-          className="corner-squircle group flex items-start justify-between rounded-xl border border-zinc-300 px-3 py-2 hover:bg-zinc-50"
-          href={`/blog/${post.slug}`}
-          key={post.slug}
-        >
-          <div className="w-full text-zinc-500 lg:flex lg:justify-between">
-            <div>
-              <p className="text-zinc-800">{post.title}</p>
-            </div>
-            <div>
-              <p className="inline font-light text-[14px]">
-                {post.publishedAt}
-              </p>
-              .&nbsp;
-              <p className="inline font-light text-[14px]">
-                {post.structuredData.wordCount} Words
-              </p>
-              , &nbsp;
-              <p className="inline font-light text-[14px]">
-                {Math.ceil(post.structuredData.wordCount / 200)} min read.
-              </p>
-            </div>
-          </div>
-        </Link>
+        <InternalLink href={`/blog/${post.slug}`} key={post.slug}>
+          {post.title}
+        </InternalLink>
       ))}
     </div>
   );
