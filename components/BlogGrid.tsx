9a017864def08ef74a8785016eec364d0782fcb2
Harits Syah <haritssr@gmail.com>
2026-01-19 06:24:13 +0700

    style(OtherLinks,BlogGrid): adjust spacing

--- components/BlogGrid.tsx
+++ components/BlogGrid.tsx
@@ -5,7 +5,7 @@ import InternalLink from "./InternalLink";
 
 export default function BlogGrid() {
   return (
-    <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid gap-3 sm:gap-x-5 sm:gap-y-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {allBlogs.map((post) => (
         <InternalLink href={`/blog/${post.slug}`} key={post.slug}>
           {post.title}
